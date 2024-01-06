31ca16437984b65bde5f52ed8652b3d838a83261
lyh <lyh@hhh.com>
2024-01-06 13:28:57 +0800

    update

--- configs/_base_/models/swin_beit.py
+++ configs/_base_/models/swin_beit.py
@@ -22,25 +22,21 @@ model = dict(
     neck=dict(type='Feature2Pyramid', embed_dim=768, rescales=[4, 2, 1, 0.5]),
     decode_head=dict(
         type='SwinHead',
-        # in_channels=3,
-        embed_dims=768, # TODO start: model args
-        # depths=[2, 2, 2, 2], # TODO: out of memory
+        embed_dims=768,
         depths=[1, 1, 3, 1],
         num_heads=[3, 6, 12, 24],
         window_size=7,
         drop_path_rate=0.1,
         use_abs_pos_embed=False,
         patch_norm=True,
-        mlp_ratio=4, # TODO end
-        in_channels=[768, 768, 768, 768], # TODO start: head args
-        # in_index=[3, 2, 1, 0], # reverse order
+        mlp_ratio=4,
+        in_channels=[768, 768, 768, 768],
         in_index=[0, 1, 2, 3], # normal order
         channels=768,
         dropout_ratio=0.1,
         num_classes=150,
-        # norm_cfg=norm_cfg,
         act_cfg=dict(type='GELU'),
-        align_corners=False, # TODO end
+        align_corners=False,
         loss_decode=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     auxiliary_head=dict(
